20644d0a0572fd6b568039083cace0501f51cb92
sonukumaryadav3207 <sonukumaryadav3207@gmail.com>
2025-11-24 19:29:45 +0530

    upadte

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,8 +1,17 @@
-<?php 
+<?php
 class Admin extends MY_Controller
 {
-    public function index(){
-        echo 'Admin';
+    public function index()
+    {
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('username', 'User Name', 'required|alpha');
+        $this->form_validation->set_rules('password', 'Password', 'required|max_length[4] ');
+        $this->form_validation->set_error_delimiters("<div class='text-danger'>", "</div");
+        if ($this->form_validation->run()) {
+            echo 'validation successful';
+        } else {
+            validation_errors();
+            $this->load->view('Users/article_list');
+        }
     }
 }
-?>
--- application/views/Users/article_list.php
+++ application/views/Users/article_list.php
@@ -1,22 +1,44 @@
 <?php include('header.php') ?>
 <div class="container" style="margin-top: 20px;">
     <h1>Admin Form</h1>
-
     <?php echo form_open('admin/index'); ?>
 
-    <div class="mb-3">
-        <label for="username" class="form-label">Username</label>
-        <?php echo form_input(['class' => 'form-control', 'placeholder' => 'enter username']); ?>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="mb-3">
+                <label class="form-label">Username</label>
+                <?php echo form_input([
+                    'class' => 'form-control',
+                    'placeholder' => 'enter username',
+                    'name' => 'username',
+                    'value' => set_value('username')
+                ]); ?>
+            </div>
+        </div>
+        <div class="col-lg-6" style="margin-top:40px;">
+            <?php echo form_error('username', "<div class='text-danger'>", "</div>"); ?>
+        </div>
     </div>
 
-    <div class="mb-3">
-        <label for="password" class="form-label">Password</label>
-        <?php echo form_password(['class' => 'form-control', 'placeholder' => 'enter password']) ?>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="mb-3">
+                <label class="form-label">Password</label>
+                <?php echo form_password([
+                    'class' => 'form-control',
+                    'placeholder' => 'enter password',
+                    'name' => 'password'
+                ]); ?>
+            </div>
+        </div>
+        <div class="col-lg-6" style="margin-top:40px;">
+            <?php echo form_error('password', "<div class='text-danger'>", "</div>"); ?>
+        </div>
     </div>
 
-    <?php echo form_submit(['type'=>'submit' ,'class'=>'btn btn-primary','value'=>'Submit']) ?>
+    <?php echo form_submit(['class' => 'btn btn-primary', 'value' => 'Submit']); ?>
+    <?php echo form_reset(['class' => 'btn btn-danger', 'value' => 'Reset']); ?>
 
-    <?php echo form_submit(['type'=>'submit' ,'class'=>'btn btn-danger','value'=>'Reset']) ?>
     </form>
 </div>
 <?php include('footer.php') ?>
